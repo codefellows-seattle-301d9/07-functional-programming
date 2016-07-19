faa2d3b78ebb5bbe6ee6199f289a2a71c7380222
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-07-19 16:58:57 -0700

    wrapped article.js in an IIFE

--- starter-code/scripts/views/adminPage.js
+++ starter-code/scripts/views/adminPage.js
@@ -1,1 +1,8 @@
+article.initAdminPage = function() {
+	var template = Handlebars.compile($('#author-template').html());
 
+	Article.numWordsByAuthor().forEach(function(stat) {
+		$('.author-stats').append(template(stat));
+	})
+	$('blog-stats .articles').text(Article.allArticles.length);
+};
