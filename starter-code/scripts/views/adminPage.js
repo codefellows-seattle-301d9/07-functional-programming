c63cc7a7bc6486ee712ca5d8d16833df1fd1bd67
David Smith <dbsmith.dbs83@gmail.com>
2016-07-20 09:24:05 -0700

    added admin stuff for page

--- starter-code/scripts/views/adminPage.js
+++ starter-code/scripts/views/adminPage.js
@@ -1,8 +1,10 @@
-article.initAdminPage = function() {
-	var template = Handlebars.compile($('#author-template').html());
-
+articleView.initAdminPage = function() {
+  var template = Handlebars.compile($('#author-template').html());
 	Article.numWordsByAuthor().forEach(function(stat) {
 		$('.author-stats').append(template(stat));
-	})
-	$('blog-stats .articles').text(Article.allArticles.length);
+	});
+  $('#blog-stats .words').text(Article.numWordsAll());
+	$('#blog-stats .articles').text(Article.allArticles.length);
 };
+
+Article.fetchAll(articleView.initAdminPage);
